41185a0eec05cb897802e2df32145bd567783b42
Chaosbohne <muhai@gmx.de>
2014-08-21 12:54:46 +0000

    first commit

---
+++ wdcm-lession-1-step-1/client/app.js
@@ -0,0 +1,3 @@
+Meteor.startup(function() {
+  Session.setDefault('wdcm', 'A reactive date source');
+});
--- wdcm-lession-1-step-1/client/views/hello/hello.js
+++ wdcm-lession-1-step-1/client/views/hello/hello.js
@@ -1,11 +1,28 @@
-  Template.hello.greeting = function () {
-    return "Welcome to wdcm.";
-  };
+Template.hello.events({
+  'click input': function () {
+    // template data, if any, is available in 'this'
+    if (typeof console !== 'undefined')
+      console.log("You pressed the button");
+    
+    Session.set('wdcm', 'Fancy things');
+  }
+});
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+Template.hello.helpers({
+  greeting: function() {
+    return "Welcome to wdcm.";
+  },
+  reactive: function() {
+    return Session.get('wdcm');
+  }
+  
+});
+
+Template.hello.created = function() {
+};
+
+Template.hello.rendered = function() {
+};
+
+Template.hello.destroyed = function() {
+};
